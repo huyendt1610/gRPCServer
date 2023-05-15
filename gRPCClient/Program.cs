ce5482b905a4201feb445f1accf635e6b48f0514
Huyendt1610 <huyen061610@gmail.com>
2023-05-15 13:18:03 +0700

    Add project files.

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using System;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001/");
+            var client = new PopulationProvider.PopulationProviderClient(channel);
+
+            Console.WriteLine(client.GetPopulation( new PopulationRequest { State = "NY"}));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gRPCServer/PopulationService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gRPCServer
+{
+    public class PopulationService: PopulationProvider.PopulationProviderBase //Grpc.Tools will generate from protobuf to c# code
+    {
+        public override Task<PopulationResponse> GetPopulation(PopulationRequest request, ServerCallContext context) {
+            return Task.FromResult(new PopulationResponse() { Count = "2000" });
+        }
+    }
+}
